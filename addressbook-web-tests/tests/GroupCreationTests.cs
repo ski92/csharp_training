f98c1532f0a6aa74fed2aa3ce300ced3998e6476
ski92 <marymadso88@gmail.com>
2019-07-25 22:39:59 +0500

    Лекция 6.2. Чтение и запись файлов в формате XML

--- addressbook-web-tests/tests/GroupCreationTests.cs
+++ addressbook-web-tests/tests/GroupCreationTests.cs
@@ -1,5 +1,7 @@
 ﻿using NUnit.Framework;
 using System;
+using System.Xml;
+using System.Xml.Serialization;
 using System.Collections.Generic;
 using System.IO;
 
@@ -23,7 +25,7 @@ namespace WebAddressbookTests
             return groups;
         }
 
-        public static IEnumerable<GroupData> GroupDataFromFile()
+        public static IEnumerable<GroupData> GroupDataFromCsvFile()
         {
             List<GroupData> groups = new List<GroupData>();
             string[] lines = File.ReadAllLines(@"groups.csv");
@@ -39,7 +41,14 @@ namespace WebAddressbookTests
             return groups;
         }
 
-        [Test, TestCaseSource("GroupDataFromFile")]
+        public static IEnumerable<GroupData> GroupDataFromXmlFile()
+        {
+            return (List<GroupData>)
+                new XmlSerializer(typeof(List<GroupData>))
+                .Deserialize(new StreamReader(@"groups.xml"));
+        }
+
+        [Test, TestCaseSource("GroupDataFromXmlFile")]
         public void GroupCreationTest(GroupData group)
         {
             List<GroupData> oldGroups = app.Groups.GetGroupLists();
